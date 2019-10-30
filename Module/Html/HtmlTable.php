e14de4fcb36b9ac8fa753b7019c5b2ed4f85cb85
inhayun <dlsgk4564@naver.com>
2019-10-30 15:29:33 +0900

    sql desc

---
+++ Module/Html/HtmlTable.php
@@ -0,0 +1,27 @@
+<?php
+namespace Module\Html;
+class HtmlTable
+{
+    function table($rows)
+    {
+        $body = "<table class=\"table\">";
+        $body .= "<thead>";
+        $body .= "<tr>
+            <th>번호</th>
+            <th>테이블명</th>
+        </tr>";
+        $body .= "</thead>";
+        $body .= "<tbody>";
+        
+        // https://github.com/infohojin/php_www
+        for($i=0;$i<count($rows);$i++) {
+            $body .= "<tr>";
+            $body .= "<td>$i</td>";
+            $body .= "<td><a href='/TableInfo/".$rows[$i]->Tables_in_php."'>".$rows[$i]->Tables_in_php."</a></td>";
+            $body .= "</tr>";
+        }    
+        $body .= "</tbody>";
+        $body .= "</table>";
+        return $body;
+    }
+}
